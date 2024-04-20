91bcb95acb89185db3e0f106a2eeb58b708dff1c
Carlos Pérez <112297700+CarloEPG@users.noreply.github.com>
2024-04-20 04:21:11 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+(function() {
+  "use strict";
+
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 45) {
+            $('.nav-bar').addClass('sticky-top');
+        } else {
+            $('.nav-bar').removeClass('sticky-top');
+        }
+    });
+ 
+   
+
+    // Header carousel
+    $(".hero-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+  /**
+   * Preloader
+   */
+  const preloader = document.querySelector('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove();
+    });
+  }
+
+    /**
+   * Initiate pURE cOUNTER
+   */
+  new PureCounter();
+
+
+
+
+})()
